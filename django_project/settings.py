127090ebc886eea1fc9722907ae69efd0b7dfcb1
Trey DeaBueno <TreyRuffy@users.noreply.github.com>
2023-11-26 14:56:18 -0700

    possible fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join('static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
