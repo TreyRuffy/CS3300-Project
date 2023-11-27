597575d41caac9e7ef049c99fb4d000ac67d1c08
Trey DeaBueno <TreyRuffy@users.noreply.github.com>
2023-11-26 21:38:11 -0700

    selenium and remove code coverage

--- cs3300_project/test_selenium.py
+++ cs3300_project/test_selenium.py
@@ -6,7 +6,7 @@ class SeleniumTesting(TestCase):
     def setUp(self):
         print(f"Testing create and delete horse")
 
-    def test_create_delete_horse(self):
+    def test_create_horse(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:8000/logout")
         driver.implicitly_wait(0.5)
@@ -24,6 +24,19 @@ class SeleniumTesting(TestCase):
         driver.find_element(By.ID, "id_short_description").send_keys("short description")
         driver.find_element(By.ID, "id_long_description").send_keys("long horse description")
         driver.find_element(By.CSS_SELECTOR, ".btn-neutral").click()
+        driver.implicitly_wait(0.5)
+    
+    def test_delete_horse(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:8000/logout")
+        driver.implicitly_wait(0.5)
+        driver.get("http://127.0.0.1:8000/login")
+        driver.implicitly_wait(0.5)
+        driver.set_window_size(787, 816)
+        driver.find_element(By.CSS_SELECTOR, ".h-5").click()
+        driver.find_element(By.NAME, "username").send_keys("testing123")
+        driver.find_element(By.NAME, "password").send_keys("happypass1")
+        driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
         driver.implicitly_wait(0.5)
         driver.find_element(By.LINK_TEXT, "horse test").click()
         driver.find_element(By.LINK_TEXT, "Delete").click()
